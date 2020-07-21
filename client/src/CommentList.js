77e118a8bf0a19e40723f709815810138c71a289
Anthony Vest <anthony.vest@outlook.com>
2020-07-21 10:22:54 +0100

    Re added client

---
+++ client/src/CommentList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const CommentList = ({ comments }) => {
+  const renderedComments = comments.map(c => {
+    const { style, text } = renderedContent(c);
+    return <li style={style} key={c.id}>{ text }</li>;
+  });
+  return <ul>{renderedComments}</ul>;
+};
+
+
+function renderedContent(comment) {
+  if(comment.status === 'pending')
+    return { style: { color: 'blue'}, text: 'Awaiting approval' };
+    
+  if(comment.status === 'rejected')
+    return { style: { color: 'red'}, text: 'Rejected' };
+
+  return { style: { color: 'green'}, text: comment.content };
+}
+
+export default CommentList;
